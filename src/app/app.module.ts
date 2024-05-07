4752f8e47b3e564589492267284e2067e5d001eb
Arshiya3010 <23pds03@wcc.edu.in>
2024-05-07 16:24:03 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SearchBarComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
